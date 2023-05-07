f7e1fe58d6076006425ed26c7c1ae36bfa4f5e8f
BuildTools <unconfigured@null.spigotmc.org>
2023-05-08 05:57:03 +0800

    Data: 20230508 Detail:     1. Add Read_FFT_File subroutine (To fine FFT 5 peak depth value)     2. linear_closest_search (To find the number is the closest number with pred in list)     3. Add Depth Correction function     4. Successful Output file

--- SNR_Correction.py
+++ SNR_Correction.py
@@ -1,22 +1,110 @@
 import Subroutine as S
+import pandas as pd
 
-ExcelKeyWord = ['Slot', 'Slot']
-
+TSV = 'S15_3_1'
 xlsFileName = S.FindFile('Slot','Slot','xls')
+OutputFileName = TSV + '_Depth_Correction.xlsx'
 
-df = S.ReadDepthFile(xlsFileName[0])
-
+df = S.Read_Excel_File(xlsFileName[0])
 DepthData = df.values.tolist()
-for lst in DepthData:
+
+for EveryDieList in DepthData:
+    #計算四捨五入的X座標與Y座標
     for i in range(2):
-        lst[i] = round(lst[i])
+        EveryDieList[i] = round(EveryDieList[i])
+    #將不在die上的Depth與SNR變None
+    loc = DepthData.index(EveryDieList)
+    if loc in S.N_ADieloc:
+        for i in range(2,4):
+            EveryDieList[i] = None
 
-i = 0 
+#計算TSV不含None點的平均SNR
+AvgSNR = sum(EveryDieList[3] for EveryDieList in DepthData if EveryDieList[3] is not None) / len([EveryDieList for EveryDieList in DepthData if EveryDieList[3] is not None])
 
-for lst in DepthData:
-    i = i+1
-    Y_SPTFileName =  S.FindFile(str(lst[0]),'spt','spt')
-    X_FFTFileName =  S.FindFile(str(lst[1]),'FFT','spt')
-    Common = S.find_common_element(X_FFTFileName, Y_SPTFileName)   
-    print('The '+ str(i) + ' pair :', end='')
-    print(Common)
+
+# Find the same file
+PairFileNameList = []
+for EveryDieList in DepthData:
+    X_SPTFileName =  S.FindFile(str(EveryDieList[0]),'spt','spt')
+    Y_FFTFileName =  S.FindFile(str(EveryDieList[1]),'FFT','spt')
+    Common = S.find_common_element(X_SPTFileName, Y_FFTFileName)   
+    PairFileNameList.append(Common)
+
+
+
+for EveryDieList in DepthData:
+    loc = DepthData.index(EveryDieList)
+    # 1. 確認SNR大小是否在平均之上
+    if EveryDieList[3] == None:
+        pass
+    elif EveryDieList[3] > AvgSNR:
+        pass
+    elif EveryDieList[3] <= AvgSNR:
+        EveryDieList[2] = 'unknown'
+        EveryDieList[3] = 'unknown'
+
+#Depths = []
+#SNRs = []
+#for EveryDieList in DepthData:
+#    Depths.append(EveryDieList[2])
+#    Depths.append(EveryDieList[3])
+
+
+
+# 修正Depth數值並標記SNR較低的點
+marks = []
+for EveryDieList in DepthData:
+    if  EveryDieList[3] == 'unknown':
+
+        marks.append(1)
+
+        loc = DepthData.index(EveryDieList)
+        Top5_Peak_Depth = S.Find_Top5_Peak_Depth(PairFileNameList[loc][1])
+
+        #找到"前"一個非None的Depth值
+        prev_depth_loc = loc - 1
+        prev_depth = None
+        if prev_depth_loc >= 0:
+            while (prev_depth == None) or (prev_depth == 'unknown'):
+                prev_depth = DepthData[prev_depth_loc][2]
+                prev_depth_loc = prev_depth_loc - 1
+                #當往前超出範圍時
+                if prev_depth_loc <= 0:
+                    break
+
+        #找到"後"一個非None的Depth值
+        next_depth_loc = loc + 1
+        next_depth = None
+        if next_depth_loc <= (S.Die_Num - 1):
+            while (next_depth == None) or (next_depth == 'unknown'):
+                next_depth = DepthData[next_depth_loc][2]
+                next_depth_loc = next_depth_loc + 1
+                #當往後超出範圍時
+                if next_depth_loc >= (S.Die_Num - 1):
+                    break
+
+
+        if (prev_depth == None) | (prev_depth == 'unknown'):
+            pred = next_depth
+        elif (next_depth == None) | (next_depth == 'unknown'):
+            pred = prev_depth
+        else:
+            pred = (prev_depth + next_depth)/2
+        
+        pred = S.linear_closest_search(Top5_Peak_Depth, pred)
+        EveryDieList[2] = pred
+    else:
+        marks.append(0)
+    
+Depths = []
+for EveryDieList in DepthData:
+    if EveryDieList[2] == None:
+        Depths.append('NA')
+    else:
+        Depths.append(EveryDieList[2])
+
+# 將list轉換為pandas DataFrame物件
+df = pd.DataFrame({'Depth': Depths, 'Mark': marks})
+
+# 將DataFrame存成Excel檔案
+df.to_excel(OutputFileName, index=False)
--- Subroutine.py
+++ Subroutine.py
@@ -1,19 +1,15 @@
 import pandas as pd
 import fnmatch
+import heapq
 import os
 
-#def FindFile(Filepath, Name, Extension):
-    # 獲取當前資料夾路徑
-    #dir_path = os.getcwd()
-    # 使用os.listdir函數獲取資料夾中的所有檔案
+Die_Num = 169
+N_ADieloc = [   0,   1,   2,  13,  14,  26,  #右上
+               10,  11,  12,  24,  25,  38,  #右下
+              156, 143, 130, 157, 144, 158,  #左上
+              166, 167, 154, 168, 155, 142,  #左下
+               19,  79,  89, 149]            #右上下左 None Die
 
-    #files = os.listdir(dir_path + Filepath)
-
-    # 遍歷所有檔案，篩選出.xls檔案
-    #for file in files:
-    #    if file.endswith(Extension):
-    #        # 顯示檔案路徑
-    #        return file
 
 def FindFile(string1, string2, extension):
     Filelist = []
@@ -23,18 +19,70 @@ def FindFile(string1, string2, extension):
                 Filelist.append(os.path.join(root, filename))
     return Filelist
 
-def ReadDepthFile(FilePath):
+
+def Read_Excel_File(FilePath):
     # 使用pandas的read_excel函數讀取Excel檔案
     df = pd.read_excel(FilePath, header=1)
 
     # 選擇列標題為“X”、“Y”、“Z”的列
     columns = df[['X', 'Y', 'Thickness', 'SNR']]
 
-    # 顯示前五個X、Y、Z列的值
     return columns
 
+
+# 找到兩個list中有相同字串的兩個元素
 def find_common_element(list1, list2):
     for element1 in list1:
         for element2 in list2:
             if element1.find(element2.split('_')[2]) != -1:
                 return [element1, element2]
+
+
+#讀取FFT Value並返回前三個對應peak的深度
+def Read_FFT_File(FilePath):
+    Depth = []
+    PeakValue = []
+    with open(FilePath, "r") as file:
+        for i in range(7):
+            file.readline()
+        for line in file:
+            x_val, y_val = line.split(maxsplit=1)
+            Depth.append(float(x_val))
+            PeakValue.append(float(y_val))
+    
+    
+def Find_Top5_Peak_Depth(FilePath):
+    Depth = []
+    PeakValue = []
+    with open(FilePath, "r") as file:
+        for i in range(7):
+            file.readline()
+        for line in file:
+            x_val, y_val = line.split(maxsplit=1)
+            Depth.append(float(x_val))
+            PeakValue.append(float(y_val))
+    file.close()
+
+    while PeakValue and PeakValue[0] == 0:
+        PeakValue.pop(0)
+        Depth.pop(0)
+
+    while PeakValue and PeakValue[-1] == 0:
+        PeakValue.pop(-1)
+        Depth.pop(-1)
+
+    Top5_Peak_val = heapq.nlargest(5, PeakValue)
+    Top5_Peak_indexes = [i for i in range(len(PeakValue)) if PeakValue[i] in Top5_Peak_val]
+    Top5_Peak_Depth = [Depth[i] for i in Top5_Peak_indexes]
+    return Top5_Peak_Depth
+
+
+def linear_closest_search(arr, x):
+    closest = arr[0]
+    diff = abs(x - closest)
+    for i in range(1, len(arr)):
+        curr_diff = abs(x - arr[i])
+        if curr_diff < diff:
+            closest = arr[i]
+            diff = curr_diff
+    return closest
